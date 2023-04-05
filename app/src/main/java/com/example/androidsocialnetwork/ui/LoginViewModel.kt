960d19db1e488fcf54a6e08c659c1494ac2f0ccb
Andrei <andrei.indosov@gmail.com>
2023-04-06 00:37:31 +0600

    deleted credentials

--- app/src/main/java/com/example/androidsocialnetwork/ui/LoginViewModel.kt
+++ app/src/main/java/com/example/androidsocialnetwork/ui/LoginViewModel.kt
@@ -10,11 +10,11 @@ import javax.inject.Inject
 
 class LoginViewModel(private val networkService: NetworkService): ViewModel() {
 
-    private val _login: MutableLiveData<String> = MutableLiveData("andrei.indosov@gmail.com")
+    private val _login: MutableLiveData<String> = MutableLiveData("")
     val login: LiveData<String> = _login
 
 
-    private val _password: MutableLiveData<String> = MutableLiveData("37EhB9B54pcUjUa")
+    private val _password: MutableLiveData<String> = MutableLiveData("")
     val password: LiveData<String> = _password
 
     private val _result: MutableLiveData<String> = MutableLiveData("")
